7fcbc4998e29f0e5644d20a6416e19c2186f34b0
ayayamad <kurimaru0105@gmail.com>
2025-04-26 11:42:34 +0900

    isalpha

---
+++ Makefile
@@ -0,0 +1,5 @@
+NAME = libft.a
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+SRCS = ft_strlen.c
+OBJS = $(SRCS:.c=.o)
---
+++ libft.h
@@ -0,0 +1,9 @@
+#ifdef LIBFT_H
+#define LIBFT_H
+
+#include <unistd.h>
+#include<stdlib.h>
+
+int ft_strlen(const char *s);
+
+#endif
