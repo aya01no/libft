fdf3f31888734bb51ef65050a83c393bc3206ac5
ayayamad <kurimaru0105@gmail.com>
2025-04-29 17:31:26 +0900

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayayamad <ayayamad@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-04-29 08:15:54 by ayayamad          #+#    #+#             */
+/*   Updated: 2025-04-29 08:15:54 by ayayamad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char * ucs1;
+	unsigned char * ucs2;
+
+	ucs1 = (unsigned char *)s1;
+	ucs2 = (unsigned char *)s2;
+	while (n > 0)
+	{
+		if (*ucs1 != *ucs2)
+		{
+			return ((*ucs1 - *ucs2));
+		}
+		else
+		{
+			ucs1++;
+			ucs2++;
+		}
+		n--;
+	}
+	return (0);
+}
