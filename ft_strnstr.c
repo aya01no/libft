62a9eef0e78cf666a49cdc8a81a0a2e3ab0827f2
ayayamad <kurimaru0105@gmail.com>
2025-05-01 10:17:56 +0900

    strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayayamad <ayayamad@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-04-29 06:41:19 by ayayamad          #+#    #+#             */
+/*   Updated: 2025-04-29 06:41:19 by ayayamad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (*little == '\0')
+		return ((char *)big);
+	i = 0;
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (big[i + j] && little[j] && big[i + j] == little[j]
+			&& i + j < len)
+		{
+			j++;
+		}
+		if (little[j] == '\0')
+		{
+			return ((char *)&big[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
