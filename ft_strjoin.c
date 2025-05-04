8f8aab6c253b949686459ce9bf05728a4c85efbf
ayayamad <kurimaru0105@gmail.com>
2025-05-04 18:13:47 +0900

    strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayayamad <ayayamad@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-05-04 04:25:54 by ayayamad          #+#    #+#             */
+/*   Updated: 2025-05-04 04:25:54 by ayayamad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char		*dst;
+	size_t		i;
+	size_t		j;
+
+	dst = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(*s1));
+	i = 0;
+	while (i < ft_strlen(s1))
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < ft_strlen(s2))
+	{
+		dst[i + j] = s2[j];
+		j++;
+	}
+	dst[i + j] = '\0';
+	return (dst);
+}
