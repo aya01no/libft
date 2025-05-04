f053a1ad90517915a60e0c64ab91840d6cba547a
ayayamad <kurimaru0105@gmail.com>
2025-05-04 21:08:04 +0900

    norm

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,9 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
