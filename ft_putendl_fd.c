f8510f3982091829156d5e55445cb6593c38d3a2
ayayamad <kurimaru0105@gmail.com>
2025-05-07 20:40:24 +0900

    putendl

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -5,7 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayayamad <ayayamad@student.42tokyo.jp>     #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-05-04 10:37:25 by ayayamad          #+#    #+#             */
-/*   Updated: 2025-05-04 10:37:25 by ayayamad         ###   ########.fr       */
+/*   Created: 2025-05-07 11:35:37 by ayayamad          #+#    #+#             */
+/*   Updated: 2025-05-07 11:35:37 by ayayamad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	int		i;
+	char	nl;
+
+	i = 0;
+	while (s[i])
+	{
+		write (fd, &s[i], 1);
+		i++;
+	}
+	nl = '\n';
+	write (fd, &nl, 1);
+}
