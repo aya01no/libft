ad16cc20c0fef4d3a746f2b8a4e0050e3f083e22
ayayamad <kurimaru0105@gmail.com>
2025-05-10 10:33:31 +0900

    striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -5,7 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayayamad <ayayamad@student.42tokyo.jp>     #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-05-04 10:37:50 by ayayamad          #+#    #+#             */
-/*   Updated: 2025-05-04 10:37:50 by ayayamad         ###   ########.fr       */
+/*   Created: 2025-05-07 10:28:47 by ayayamad          #+#    #+#             */
+/*   Updated: 2025-05-07 10:28:47 by ayayamad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		(*f)(i, &s[i]);
+		i++;
+	}
+}
