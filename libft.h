32594e91e8e6328ce5a0763cdf19c05269cc1ab9
ayayamad <kurimaru0105@gmail.com>
2025-05-10 11:03:50 +0900

    libft bonus norm

--- libft.h
+++ libft.h
@@ -54,8 +54,8 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 typedef struct s_list
 {
-	void *content;
-	struct s_list *next;
-} t_list;
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
 #endif
