3b8cb623622318c70afa9262ba9b7292deb5e466
ayayamad <kurimaru0105@gmail.com>
2025-05-10 20:43:20 +0900

    memcpy_null

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*dest_cpy;
 	unsigned char	*src_cpy;
 
+	if (!dest && !src)
+		return (NULL);
 	dest_cpy = (unsigned char *)dest;
 	src_cpy = (unsigned char *)src;
 	while (n--)
