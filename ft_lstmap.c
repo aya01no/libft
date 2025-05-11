dcac1801d71dd21edc201125507ba7e7b3454210
ayayamad <kurimaru0105@gmail.com>
2025-05-11 14:03:34 +0900

    lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -16,17 +16,21 @@ t_list	*ft_lstmap(t_list *lst, void*(*f)(void *), void(*del)(void *))
 {
 	t_list	*new_lst;
 	t_list	*new_node;
+	void	*new_content;
 
 	if (!lst)
 		return (NULL);
 	new_lst = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		new_content = f(lst->content);
+		if (!new_content)
+			return (ft_lstclear(&new_lst, del), NULL);
+		new_node = ft_lstnew(new_content);
 		if (!new_node)
 		{
-			ft_lstclear(&new_lst, (*del));
-			return (NULL);
+			del(new_content);
+			return (ft_lstclear(&new_lst, del), NULL);
 		}
 		ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
